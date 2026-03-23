1c014717e2d4bc2c619e5d711759d927fc824782
Chuong Mai <codewithintention@gmail.com>
2026-03-23 11:48:04 -0400

    Update hello.py
    Add complete program lines

--- code/chapter-01/hello.py
+++ code/chapter-01/hello.py
@@ -1,1 +1,9 @@
-print("Hello, world!")
+print("Hello")
+
+name = input("What is your name? ")
+print("Glad to meet you,", name)
+
+time = input("What time is it? ")
+print(time, "already?")
+
+print("I'm late for work! Goodbye,", name)
