17f6ad58cf5742a4b7ed4abbf27ea049de35a44e
Chuong Mai <codewithintention@gmail.com>
2026-04-14 16:01:16 -0400

    Implement number guessing game v6

---
+++ code/chapter-07/guess_game_v6.js
@@ -0,0 +1,31 @@
+const MIN_NUMBER = 1;
+const MAX_NUMBER = 100;
+
+console.log("Welcome! Let's play the number guessing game.\nVersion 6\n");
+
+let secret_number = Math.floor(Math.random() * MAX_NUMBER) + MIN_NUMBER;
+let guess = null;
+let tries = 0;
+
+// Game loop
+while (guess !== secret_number) {
+    let user_input = await input(`Guess a number from ${MIN_NUMBER} to ${MAX_NUMBER}?`);
+    guess = Number(user_input);
+    
+    // Count the number of tries
+    tries += 1;
+    
+    if (guess === secret_number) {
+        if (tries === 1) {
+            console.log("Wow! You guessed it on the first try!"); // Step 4a
+        } else {
+            console.log(`You guessed it in ${tries} tries!`); // Step 4b
+        }
+    } else if (guess < secret_number) {
+        console.log("Higher. Guess again.");
+    } else {
+        console.log("Lower. Guess again.");
+    }
+}
+
+console.log("Goodbye.");
