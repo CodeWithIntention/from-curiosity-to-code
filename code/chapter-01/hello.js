d2e79f986a600536c4f7ecc59c58dee90675a7ee
Chuong Mai <codewithintention@gmail.com>
2026-05-01 19:25:34 -0400

    Remove extra spaces in prompt messages

--- code/chapter-01/hello.js
+++ code/chapter-01/hello.js
@@ -1,9 +1,9 @@
 console.log("Hello");
 
-let user = prompt("What is your name? ");
+let user = prompt("What is your name?");
 console.log("Glad to meet you,", user);
 
-let time = prompt("What time is it? ");
+let time = prompt("What time is it?");
 console.log(time, "already?");
 
 console.log("I'm late for work! Goodbye,", user);
